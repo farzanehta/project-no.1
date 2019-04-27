1863a10b3070bc8daf27517c76e2942d3d569b2f
farzane.talebi <farzaneh.talebi115@gmail.com>
2019-04-27 10:54:07 +0430

    Question2. revised

--- assignment02.py
+++ assignment02.py
@@ -1,29 +1,22 @@
 unit = input("Are you using standard units(y/n)? ").lower()
 weight = int(input("weight: "))
 height = float(input("height: "))
-BMI_SD = weight / (height ** 2)
-BMI_non = (weight / (height ** 2)) * 703
+BMI = weight / (height ** 2)
 
-if unit == "y":
-    print(BMI_SD)
-    if BMI_SD < 18.5:
+
+if unit == "n":
+    BMI = BMI * 703
+
+    print(BMI)
+    if BMI < 18.5:
         print("Underweight")
-    if 18.5 < BMI_SD < 25:
+    if 18.5 < BMI < 25:
         print("Normal")
-    if 25 < BMI_SD < 30:
+    if 25 < BMI < 30:
         print("Overweight")
-    if BMI_SD > 30:
+    if BMI > 30:
         print("Obesity")
-elif unit == "n":
-    print(BMI_non)
-    if BMI_non < 18.5:
-        print("Underweight")
-    if 18.5 < BMI_non < 25:
-        print("Normal")
-    if 25 < BMI_non < 30:
-        print("Overweight")
-    if BMI_non > 30:
-        print("Obesity")
+
 else:
     print("sry, something is wrong!")
 
