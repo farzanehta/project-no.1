1371e40d0732aea836000ccd2871e5a493f19576
farzane.talebi <farzaneh.talebi115@gmail.com>
2019-04-28 11:34:59 +0430

    Question3

--- assignment01/Question3.py
+++ assignment01/Question3.py
@@ -3,3 +3,8 @@ left = lst[:5] + lst[4::-1]
 right = lst[-1:-6:-1] + lst[5:]
 print(left)
 print(right)
+
+#for myself
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+reverse_mid = lst[7:3:-1]
+print(reverse_mid)
