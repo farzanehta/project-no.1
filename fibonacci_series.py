9f1c1d82c33e38071b0c94c11bf3fd40328a2c39
farzane.talebi <farzaneh.talebi115@gmail.com>
2019-05-06 12:53:37 +0430

    fibonacci by while

---
+++ fibonacci_series.py
@@ -0,0 +1,16 @@
+# 1
+lst = [1, 1]
+i = 2
+
+while i < 22:
+    new_member = lst[i - 1] + lst[i - 2]
+    lst = lst + [new_member]
+    i += 1
+    print(lst)
+
+# 2
+seq = [1, 1]
+while len(seq) < 22:
+    new_member = seq[-1] + seq[-2]
+    seq.append(new_member)
+    print(seq)
