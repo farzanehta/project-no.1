c5e9afe7c1d33da2425aed943c3e028211310379
farzane.talebi <farzaneh.talebi115@gmail.com>
2019-05-06 12:11:08 +0430

    sum of num

---
+++ sum_of_num.py
@@ -0,0 +1,16 @@
+sum_of_num = 0
+i = 0
+while i < 10:
+    i += 1
+    sum_of_num = sum_of_num + i
+
+print(sum_of_num)
+
+
+sum_of_num = 0
+i = 0
+while i < 10:
+    sum_of_num = sum_of_num + i
+    i += 1
+
+print(sum_of_num)
