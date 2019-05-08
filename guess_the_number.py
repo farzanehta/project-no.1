6f8bb4f3b626c3a630b409af91ac3a363174c7fa
farzane.talebi <farzaneh.talebi115@gmail.com>
2019-05-08 14:57:01 +0430

    guess game!

---
+++ guess_the_number.py
@@ -0,0 +1,14 @@
+import random
+secret = random.randint(1,20)
+print(secret)
+guess_count = 5
+for _ in range(5) :
+    guess = int(input("guess a number: "))
+    if guess < secret:
+        print("you're guess is LESS than the secret")
+    elif guess > secret:
+        print("you're guess is GREATER than the secret")
+    else:
+        print("BINGO!")
+
+print("you're a LOSER!")
