7b9aedc03ef53599546068e5c96255054527dfdd
farzane.talebi <farzaneh.talebi115@gmail.com>
2019-05-14 14:37:22 +0430

    by function with 2 solutions

--- Mosh/whether_perfect_number.py
+++ Mosh/whether_perfect_number.py
@@ -1,6 +1,9 @@
 # what is perfect number?
 # sum of divisors (except number itself) == number OR sum of all divisors (include number itself) //2 == num
 
+# 1 >> My code
+
+
 def whether_perfect(num):
     is_perfect = False
     i = 1
@@ -17,4 +20,16 @@ def whether_perfect(num):
     return is_perfect
 
 
-print(whether_perfect(8128))
+print(whether_perfect(33550336))
+
+
+# 2 >> net code
+def perfect_number(n):
+    sum = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum += i
+    return sum == n
+
+
+print(perfect_number(33550336))
